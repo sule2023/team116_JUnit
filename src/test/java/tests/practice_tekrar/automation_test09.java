3336064d4dfa8cfda72acb60c9d61398fd69ba6c
sule2023 <ysule1014@gmail.com>
2023-07-12 09:08:20 +0200

    practieson hali

--- src/test/java/tests/practice_tekrar/automation_test09.java
+++ src/test/java/tests/practice_tekrar/automation_test09.java
@@ -9,57 +9,55 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.TestBase;
 
 import java.time.Duration;
 import java.util.List;
 
-public class automation_test09 {
+public class automation_test09 extends TestBase {
 
-    static WebDriver driver;
-    @Before
-    public void setUp(){
-        //1. Launch browser(Tarayıcıyı başlatın.)
-        WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
-    @After
-    public void tearDown(){driver.close();}
+
     @Test
     public  void Test09(){
 
         //2. Navigate to url 'http://automationexercise.com'
         driver.get("http://automationexercise.com");
+
         //3. Verify that home page is visible successfully(Ana sayfanın başarıyla görüntülendiğini doğrulayın.)
-        WebElement homePage = driver.findElement(By.xpath(" //*[text()=' Home' ]"));
-       // WebElement homepage=driver.findElement(By.xpath("//section[@id='slider']"));
-        Assert.assertTrue(homePage.isDisplayed());
+
+        WebElement homepage=driver.findElement(By.xpath("//section[@id='slider']"));
+        Assert.assertTrue(homepage.isDisplayed());
 
         //4. Click on 'Products' button('Ürünler' düğmesine tıklayın.)
-        driver.findElement(By.linkText("//i[@class='fa fa-envelope']")).click();
 
+        driver.findElement(By.xpath("//*[text()=' Products']")).click();
         //5. Verify user is navigated to ALL PRODUCTS page successfully( Kullanıcının başarıyla TÜM ÜRÜNLER sayfasına yönlendirildiğini doğrulayın.)
 
         WebElement allProducts=driver.findElement(By.className("features_items"));
         Assert.assertTrue(allProducts.isEnabled());
         //6. Enter product name in search input and click search button(Arama girişine ürün adını girin ve arama düğmesine tıklayın.)
-        WebElement searchInput = driver.findElement(By.id("searchInput"));
-        searchInput.sendKeys("ürün adı");
+        WebElement searchInput = driver.findElement(By.id("search_product"));
+        searchInput.sendKeys("etek");
 
-        WebElement searchButton = driver.findElement(By.id("searchButton"));
+        WebElement searchButton = driver.findElement(By.xpath("//button[@type='button']"));
         searchButton.click();
         //7. Verify 'SEARCHED PRODUCTS' is visible('ARANAN ÜRÜNLER' görünür olup olmadığını doğrulayın.)
-        WebElement searchedProducts = driver.findElement(By.cssSelector(".search-results"));
+        WebElement searchedProducts = driver.findElement(By.xpath("//*[text()='Searched Products']"));
+        Assert.assertTrue(searchedProducts.isDisplayed());
 
         //8. Verify all the products related to search are visible( Aramayla ilgili tüm ürünlerin görünür olduğunu doğrulayın.)
-        List<WebElement> searchResults = driver.findElements(By.cssSelector(".search-results .product"));
-        if (searchResults.size() > 0) {
-            System.out.println("Arama sonuçları görünür.");
-        } else {
-            System.out.println("Arama sonuçları görünür değil.");
+
+        WebElement allproducts=driver.findElement(By.xpath(" //*[@class='features_items']"));
+        Assert.assertTrue(allproducts.isDisplayed());
+        System.out.println("all products: test passed");
+
+
         }
 
-        driver.quit();
+
+
+
+
+
     }
-}
+
--- src/test/java/tests/practice_tekrar/automation_test10.java
+++ src/test/java/tests/practice_tekrar/automation_test10.java
@@ -4,11 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
@@ -31,33 +29,26 @@ public class automation_test10 {
         //2. Navigate to url 'http://automationexercise.com'('http://automationexercise.com' adresine gidin.)
         driver.get("http://automationexercise.com");
         //3. Verify that home page is visible successfully(Ana sayfanın başarıyla görüntülendiğini doğrulayın.)
-        String expectedTitle = "Automation Exercise";
-        String actualTitle = driver.getTitle();
-        if (actualTitle.equals(expectedTitle)) {
-            System.out.println("Ana sayfa başarıyla görüntülendi.");
-        } else {
-            System.out.println("Ana sayfa görüntülenirken bir hata oluştu.");
-        }
+        driver.findElement(By.xpath("//section[@id='slider']"));
+
+
+
         //4. Scroll down to footer(Sayfanın alt kısmına doğru kaydırın.)
+        //scrollDown(driver);
         WebElement footer = driver.findElement(By.id("footer"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", footer);
 
         //5. Verify text 'SUBSCRIPTION'('ABONELİK' metninin görünürlüğünü doğrulayın.)
         WebElement subscriptionText = driver.findElement(By.xpath("//div[contains(text(), 'SUBSCRIPTION')]"));
-        if (subscriptionText.isDisplayed()) {
-            System.out.println("'SUBSCRIPTION' metni görünür.");
-        } else {
-            System.out.println("'SUBSCRIPTION' metni görünürken bir hata oluştu.");
-        }
 
 
         //6. Enter email address in input and click arrow button(Giriş kutusuna e-posta adresini girin ve ok işaretli düğmeye tıklayın.)
-        WebElement emailInput = driver.findElement(By.id("email-input"));
-        WebElement arrowButton = driver.findElement(By.id("arrow-button"));
+       driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("abc@yahoo.com");
 
-        String emailAddress = "test@example.com";
-        emailInput.sendKeys(emailAddress);
-        arrowButton.click();
+
+
+
+
 
         //7. Verify success message 'You have been successfully subscribed!' is visible('Başarıyla abone oldunuz!' mesajının görünür olduğunu doğrulayın.)
         WebElement successMessage = driver.findElement(By.xpath("//div[contains(text(), 'You have been successfully subscribed!')]"));
@@ -69,7 +60,25 @@ public class automation_test10 {
 
 
 
+     /*
+       // 4. Adım: Sayfayı aşağı kaydırın
+        scrollDown(driver);
 
+        // 5. Adım: 'SUBSCRIPTION' metnini doğrulayın
+        Assert.assertTrue("'SUBSCRIPTION' metni görüntülenemedi.", isElementVisible(driver, By.xpath("//h2[text()='SUBSCRIPTION']")));
+
+        // 6. Adım: E-posta adresini girin ve ok düğmesine tıklayın
+        WebElement emailInput = driver.findElement(By.id("email")); // E-posta giriş alanının ID'sini buraya ekleyin
+        emailInput.sendKeys("example@example.com"); // E-posta adresini buraya ekleyin
+
+        WebElement arrowButton = driver.findElement(By.className("arrow")); // Ok düğmesinin CSS sınıfını buraya ekleyin
+        arrowButton.click();
+
+        // 7. Adım: Başarı mesajının görüntülendiğini doğrulayın
+        Assert.assertTrue("Başarı mesajı görüntülenemedi.",
+                isElementVisible(driver, By.xpath("//div[contains(text(),'You have been successfully subscribed!')]")));
+
+      */
 
 
 
